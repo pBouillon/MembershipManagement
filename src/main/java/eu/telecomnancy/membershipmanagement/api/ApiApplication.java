78c193db99fb9314bc25fe8594fd7bf6f57a43cb
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-12 15:15:51 +0100

    Add comment for the REST API entrypoint

--- src/main/java/eu/telecomnancy/membershipmanagement/api/ApiApplication.java
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/ApiApplication.java
@@ -3,9 +3,15 @@ package eu.telecomnancy.membershipmanagement.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Web API entry-point class
+ */
 @SpringBootApplication
 public class ApiApplication {
 
+	/**
+	 * Spring Web API entry-point
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
