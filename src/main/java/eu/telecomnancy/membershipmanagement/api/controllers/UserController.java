822c6e1d7b1e6d4f27ccd06146d4283e3095d745
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-12 16:56:31 +0100

    Wire the UserService to the UserController

--- src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/UserController.java
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/UserController.java
@@ -1,11 +1,16 @@
 package eu.telecomnancy.membershipmanagement.api.controllers;
 
+import eu.telecomnancy.membershipmanagement.api.domain.User;
+import eu.telecomnancy.membershipmanagement.api.services.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * API controller for the User resource
@@ -23,6 +28,21 @@ public class UserController {
     public static final String CONTROLLER_TAG = "User";
 
     /**
+     * Service to handle user-related operations
+     */
+    private final UserService userService;
+
+    /**
+     * Default controller constructor
+     *
+     * @param userService Service to handle user-related operations
+     */
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
      * Endpoint for: GET /users
      *
      * Retrieve all users of the system
@@ -32,11 +52,10 @@ public class UserController {
     @GetMapping(path = "/users")
     @ApiOperation(value="Retrieve all users of the system", tags = { CONTROLLER_TAG })
     public ResponseEntity<Object> Get() {
+        List<User> users = userService.getUsers();
+
         return ResponseEntity.ok()
-                // Create anonymous class in order to have a properly serialized JSON as a result
-                .body(new Object() {
-                    public final String message = "Hello There !";
-                });
+                .body(users);
     }
 
 }
