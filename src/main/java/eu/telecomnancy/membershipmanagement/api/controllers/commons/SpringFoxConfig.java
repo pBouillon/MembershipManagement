05622654466fba4d8ab1e592e8d54687bb785fe3
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-12 16:10:30 +0100

    Add Swagger configuration

---
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/commons/SpringFoxConfig.java
@@ -0,0 +1,32 @@
+package eu.telecomnancy.membershipmanagement.api.controllers.commons;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Swagger configuration class
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    /**
+     * Expose controllers and endpoints to be displayed by Swagger
+     *
+     * @return The associated Java Bean
+     */
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
