f0a4a9eede513ec2b0049dc24dc958385c5a8d10
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-13 18:42:05 +0100

    Rename PersonService to UserService and implement CQRS

---
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/services/user/IUserCommandService.java
@@ -0,0 +1,12 @@
+package eu.telecomnancy.membershipmanagement.api.services.user;
+
+/**
+ * Command part of the UserService
+ * Specify the write-only commands
+ *
+ * The subsequent separation is made to respect the CQRS principles
+ * see: https://www.baeldung.com/cqrs-for-a-spring-rest-api
+ *
+ * @see UserService
+ */
+public interface IUserCommandService { }
