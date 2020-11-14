e0593b22b3441b9d0465d7851f72d7a6b1a54286
VVarnier <victor_varnier@hotmail.fr>
2020-11-14 18:32:39 +0100

    Create the query for GET user/:id

---
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/queries/GetUserQuery.java
@@ -0,0 +1,16 @@
+package eu.telecomnancy.membershipmanagement.api.controllers.queries;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+// TODO Docs
+@Getter @Setter @AllArgsConstructor
+public class GetUserQuery {
+
+    /**
+     * Id of the user
+     */
+    private long id;
+
+}
