83a68c3288a980091793bf532e52ddf3fca0dfc3
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-14 15:05:08 +0100

    Create the command's mapping

--- src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/utils/mappings/UserMapper.java
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/utils/mappings/UserMapper.java
@@ -1,5 +1,6 @@
 package eu.telecomnancy.membershipmanagement.api.controllers.utils.mappings;
 
+import eu.telecomnancy.membershipmanagement.api.controllers.commands.CreateUserCommand;
 import eu.telecomnancy.membershipmanagement.api.controllers.dto.UserDto;
 import eu.telecomnancy.membershipmanagement.api.domain.User;
 import org.mapstruct.Mapper;
@@ -13,7 +14,7 @@ import java.util.List;
 public interface UserMapper {
 
     /**
-     * Convert a User entity to a User DTO
+     * Convert a {@link User} entity to a {@link UserDto}
      *
      * @param user Entity to be converted
      * @return The associated DTO
@@ -21,11 +22,19 @@ public interface UserMapper {
     UserDto toDto(User user);
 
     /**
-     * Convert user entities to a User DTOs
+     * Convert {@link User} entities to a list of {@link UserDto}
      *
      * @param users Entities to be converted
      * @return A list containing the associated DTOs
      */
     List<UserDto> toDtoList(List<User> users);
+
+    /**
+     * Convert a {@link CreateUserCommand} to a {@link User}
+     *
+     * @param command Command to be converted
+     * @return The associated User
+     */
+    User toUser(CreateUserCommand command);
 
 }
