fb500447088371295fd82573271c62ce34d792a1
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-14 15:05:50 +0100

    Create POST users/ endpoint

--- src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/user/UserWriteRestController.java
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/user/UserWriteRestController.java
@@ -1,11 +1,19 @@
 package eu.telecomnancy.membershipmanagement.api.controllers.user;
 
+import eu.telecomnancy.membershipmanagement.api.controllers.commands.CreateUserCommand;
+import eu.telecomnancy.membershipmanagement.api.controllers.dto.UserDto;
 import eu.telecomnancy.membershipmanagement.api.controllers.utils.mappings.UserMapper;
+import eu.telecomnancy.membershipmanagement.api.domain.User;
 import eu.telecomnancy.membershipmanagement.api.services.user.IUserCommandService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 /**
  * API controller for the User resource
@@ -16,7 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(
         path = "/api/users",
-        produces = "application/json")
+        produces = "application/json",
+        consumes = "application/json")
 @Api(value = "User", tags = { UserRestController.CONTROLLER_TAG })
 public class UserWriteRestController extends UserRestController {
 
@@ -36,6 +45,35 @@ public class UserWriteRestController extends UserRestController {
         super(mapper);
 
         this.userService = userService;
+    }
+
+    /**
+     * Endpoint for: POST /users
+     *
+     * Create a new user with no team
+     *
+     * @param createUserCommand A JSON payload containing all the users
+     * @return A JSON payload containing all the users
+     */
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value="Create a new user with no team")
+    public ResponseEntity<UserDto> Post(
+            @RequestBody CreateUserCommand createUserCommand) {
+        // Create the new user and retrieve the newly created one
+        User created = userService.createUser(createUserCommand);
+
+        // Extract the new user's location in the API
+        URI location
+                = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+
+        // Return the result with its location
+        return ResponseEntity.created(location)
+                .body(mapper.toDto(created));
     }
 
 }
