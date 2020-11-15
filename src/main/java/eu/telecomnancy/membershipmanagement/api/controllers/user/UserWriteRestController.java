26f6ac5e441fc270d930232046664767bc6028e0
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-15 09:35:37 +0100

    Optimize imports and code cleanup

--- src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/user/UserWriteRestController.java
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/user/UserWriteRestController.java
@@ -10,7 +10,6 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
