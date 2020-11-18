643aff9690adbef923074d8fe73eae8393cd5fb2
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-18 16:50:54 +0100

    Add Swagger annotations on UserReadRestController

--- src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/user/UserReadRestController.java
+++ src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/user/UserReadRestController.java
@@ -7,13 +7,16 @@ import eu.telecomnancy.membershipmanagement.api.controllers.utils.mappings.UserM
 import eu.telecomnancy.membershipmanagement.api.domain.User;
 import eu.telecomnancy.membershipmanagement.api.services.user.IUserQueryService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -57,8 +60,10 @@ public class UserReadRestController extends UserRestController {
      * @return A JSON payload containing all the users
      */
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value="Retrieve all users tracked by the system")
+    @Operation(summary = "Retrieve all users tracked by the system",
+             responses = {
+                     @ApiResponse(responseCode = "200", description = "Users successfully retrieved")
+             })
     public ResponseEntity<List<UserDto>> get() {
         List<User> users = userService.getUsers();
 
@@ -75,8 +80,11 @@ public class UserReadRestController extends UserRestController {
      * @return A JSON payload containing the user
      */
     @GetMapping(path = "/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    @ApiOperation(value="Retrieve an existing user by its id")
+    @Operation(summary = "Retrieve an existing user by its id",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "User successfully retrieved"),
+                    @ApiResponse(responseCode = "404", description = "User not found")
+            })
     public ResponseEntity<UserDetailsDto> getUser(
             @ApiParam(value = "Id of the user to retrieve")
             @PathVariable long id) {
