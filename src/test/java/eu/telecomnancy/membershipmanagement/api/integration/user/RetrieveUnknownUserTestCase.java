4dc74df0889630decd5ffd8da68ea6b89e183d06
VVarnier <victor_varnier@hotmail.fr>
2020-11-19 17:02:12 +0100

    Create test for the case of retrieving the list of users when no user had been created

--- src/test/java/eu/telecomnancy/membershipmanagement/api/integration/user/RetrieveUnknownUserTestCase.java
+++ src/test/java/eu/telecomnancy/membershipmanagement/api/integration/user/RetrieveUnknownUserTestCase.java
@@ -1,8 +1,7 @@
 package eu.telecomnancy.membershipmanagement.api.integration.user;
 
 import eu.telecomnancy.membershipmanagement.api.IntegrationTest;
-import eu.telecomnancy.membershipmanagement.api.controllers.team.TeamReadRestController;
-import eu.telecomnancy.membershipmanagement.api.controllers.team.TeamWriteRestController;
+import eu.telecomnancy.membershipmanagement.api.controllers.user.UserReadRestController;
 import eu.telecomnancy.membershipmanagement.api.controllers.utils.dto.user.UserDetailsDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -19,8 +18,7 @@ import java.net.URISyntaxException;
  *     (Read-only operation)
  *     Test when an id that does not correspond to an existing user is provided
  *
- * @see TeamReadRestController
- * @see TeamWriteRestController
+ * @see UserReadRestController
  */
 public class RetrieveUnknownUserTestCase extends IntegrationTest {
 
