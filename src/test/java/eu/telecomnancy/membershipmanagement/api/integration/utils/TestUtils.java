f705019a3148799efa301bee6165160562c4c0df
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-19 16:43:28 +0100

    Add test utilities

---
+++ src/test/java/eu/telecomnancy/membershipmanagement/api/integration/utils/TestUtils.java
@@ -0,0 +1,23 @@
+package eu.telecomnancy.membershipmanagement.api.integration.utils;
+
+/**
+ * Utilities for the various test cases
+ */
+public final class TestUtils {
+
+    /**
+     * Non-empty blank string
+     */
+    public static final String BLANK_STRING = "    ";
+
+    /**
+     * Empty string
+     */
+    public static final String EMPTY_STRING = "";
+
+    /**
+     * Long string, over the acceptable limits for the payload lengths
+     */
+    public static final String LONG_STRING = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+
+}
