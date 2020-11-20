cfdfec9b46ea0dfc8bdd16acd180507be011d06d
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-11-20 15:19:39 +0100

    Add test case for the team's name update

---
+++ src/test/java/eu/telecomnancy/membershipmanagement/api/integration/team/ChangeTeamNameTestCase.java
@@ -0,0 +1,74 @@
+package eu.telecomnancy.membershipmanagement.api.integration.team;
+
+import eu.telecomnancy.membershipmanagement.api.IntegrationTest;
+import eu.telecomnancy.membershipmanagement.api.controllers.team.TeamWriteRestController;
+import eu.telecomnancy.membershipmanagement.api.controllers.utils.cqrs.team.UpdateTeamCommand;
+import eu.telecomnancy.membershipmanagement.api.controllers.utils.dto.team.TeamDetailsDto;
+import eu.telecomnancy.membershipmanagement.api.controllers.utils.dto.team.TeamDto;
+import eu.telecomnancy.membershipmanagement.api.domain.Team;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Route :
+ *     /api/teams/:id
+ *
+ * Case :
+ *     (Write-only operation)
+ *     Test the team update
+ *
+ * @see TeamWriteRestController
+ */
+public class ChangeTeamNameTestCase extends IntegrationTest {
+
+    /**
+     * Ensure that the team's data are successfully updated
+     *
+     * @throws URISyntaxException Throws exception when the URI is invalid
+     */
+    @Test
+    public void updateTheTeamInformation() throws URISyntaxException {
+        // Create the team
+        Team toCreate = new Team("ApprenTeam");
+
+        URI creationUri = getUrlForRoute("/api/teams");
+
+        ResponseEntity<TeamDto> createdResponse
+                = restTemplate.postForEntity(creationUri, toCreate, TeamDto.class);
+
+        assertEquals(createdResponse.getStatusCode(), HttpStatus.CREATED);
+
+        TeamDto created = createdResponse.getBody();
+        assertNotNull(created);
+
+        // Change its name
+        UpdateTeamCommand updateTeamCommand = new UpdateTeamCommand();
+        updateTeamCommand.setName(new Date().toString());
+
+        URI updateUrl = getUrlForRoute("/api/teams/" + created.getId());
+
+        restTemplate.put(updateUrl, updateTeamCommand);
+
+        // Retrieve the team and assert that the name changed
+        URI teamUrl = getUrlForRoute("/api/teams/" + created.getId());
+
+        ResponseEntity<TeamDetailsDto> updatedResponse
+                = restTemplate.getForEntity(teamUrl, TeamDetailsDto.class);
+
+        assertEquals(updatedResponse.getStatusCode(), HttpStatus.OK);
+
+        TeamDetailsDto updated = updatedResponse.getBody();
+        assertNotNull(updated);
+
+        assertEquals(updated.getName(), updateTeamCommand.getName());
+    }
+
+}
