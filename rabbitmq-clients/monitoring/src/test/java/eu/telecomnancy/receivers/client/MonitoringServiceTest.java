b0a47e2681876cdfacca40e290fa2c66e604302f
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-12-08 16:21:47 +0100

    WiP - Create the unit tests

--- rabbitmq-clients/monitoring/src/test/java/eu/telecomnancy/receivers/client/MonitoringServiceTest.java
+++ rabbitmq-clients/monitoring/src/test/java/eu/telecomnancy/receivers/client/MonitoringServiceTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
 /**
  * Unit test suite for the TeamService
  *
@@ -29,15 +31,79 @@ public class MonitoringServiceTest {
     UserCounterService userCounterService;
 
     @Test
-    public void givenTheCreateTeamCommand_WhenUpdatingTheCounter_ThenTheTeamCountShouldBeIncremented() { }
+    public void givenTheCreateTeamCommand_WhenUpdatingTheCounter_ThenTheTeamCountShouldBeIncremented() {
+        // Arrange
+        doNothing()
+                .when(teamCounterService)
+                .increment();
+
+        String receivedCommand = TeamCounterService.INCREMENT_COUNT_OPERATION_NAME;
+
+        MonitoringService monitoringService = new MonitoringService(teamCounterService, userCounterService);
+
+        // Act
+        monitoringService.alterCountFromOperation(receivedCommand);
+
+        // Assert
+        verify(teamCounterService, times(1))
+                .increment();
+    }
 
     @Test
-    public void givenTheCreateUserCommand_WhenUpdatingTheCounter_ThenTheUserCountShouldBeIncremented() { }
+    public void givenTheCreateUserCommand_WhenUpdatingTheCounter_ThenTheUserCountShouldBeIncremented() {
+        // Arrange
+        doNothing()
+                .when(userCounterService)
+                .increment();
+
+        String receivedCommand = UserCounterService.INCREMENT_COUNT_OPERATION_NAME;
+
+        MonitoringService monitoringService = new MonitoringService(teamCounterService, userCounterService);
+
+        // Act
+        monitoringService.alterCountFromOperation(receivedCommand);
+
+        // Assert
+        verify(userCounterService, times(1))
+                .increment();
+    }
 
     @Test
-    public void givenTheDeleteTeamCommand_WhenUpdatingTheCounter_ThenTheTeamCountShouldBeDecremented() { }
+    public void givenTheDeleteTeamCommand_WhenUpdatingTheCounter_ThenTheTeamCountShouldBeDecremented() {
+        // Arrange
+        doNothing()
+                .when(teamCounterService)
+                .decrement();
+
+        String receivedCommand = TeamCounterService.DECREMENT_COUNT_OPERATION_NAME;
+
+        MonitoringService monitoringService = new MonitoringService(teamCounterService, userCounterService);
+
+        // Act
+        monitoringService.alterCountFromOperation(receivedCommand);
+
+        // Assert
+        verify(teamCounterService, times(1))
+                .decrement();
+    }
 
     @Test
-    public void givenTheDeleteUserCommand_WhenUpdatingTheCounter_ThenTheUserCountShouldBeDecremented() { }
+    public void givenTheDeleteUserCommand_WhenUpdatingTheCounter_ThenTheUserCountShouldBeDecremented() {
+        // Arrange
+        doNothing()
+                .when(userCounterService)
+                .decrement();
+
+        String receivedCommand = UserCounterService.DECREMENT_COUNT_OPERATION_NAME;
+
+        MonitoringService monitoringService = new MonitoringService(teamCounterService, userCounterService);
+
+        // Act
+        monitoringService.alterCountFromOperation(receivedCommand);
+
+        // Assert
+        verify(userCounterService, times(1))
+                .decrement();
+    }
 
 }
