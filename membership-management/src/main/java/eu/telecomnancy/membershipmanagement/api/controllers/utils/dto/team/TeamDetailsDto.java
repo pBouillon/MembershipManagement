7a524604d540cf6f464cc2f4a69825bbc23010b2
pBouillon <pro.pierre.bouillon@protonmail.com>
2020-12-09 14:46:30 +0100

    Add the team's creation date in the DTOs

--- membership-management/src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/utils/dto/team/TeamDetailsDto.java
+++ membership-management/src/main/java/eu/telecomnancy/membershipmanagement/api/controllers/utils/dto/team/TeamDetailsDto.java
@@ -1,17 +1,22 @@
 package eu.telecomnancy.membershipmanagement.api.controllers.utils.dto.team;
 
+import java.time.Instant;
+
 import eu.telecomnancy.membershipmanagement.api.controllers.utils.dto.user.UserDto;
-import eu.telecomnancy.membershipmanagement.api.domain.User;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.List;
 
 /**
  * User Data Transfer Object to be served and received by the API
  */
-@Getter @Setter
+@Data
 public class TeamDetailsDto {
+
+    /**
+     * Creation date of the team, represented as UTC
+     */
+    private Instant creationDate;
 
     /**
      * Id of the team
